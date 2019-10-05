78998bdbfcdf54f1383c2cc5d96db68267572608
Tiago Onofre <taraujonofre@gmail.com>
2019-10-04 23:47:06 -0300

    iterator and initializers

--- include/vector.h
+++ include/vector.h
@@ -3,6 +3,9 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <initializer_list>
+#include <iterator>
 
 namespace sc {
 
@@ -11,19 +14,18 @@ namespace sc {
     class vector
     {
 
+        private:
+            T *m_data; // array
+            size_t m_capacity; // logical capacity
+            size_t m_end; // logical index
+
         public:
-        //private:
-            T *m_data;
-            size_t m_capacity;
-            size_t m_end;
 
-        //public:
+            // CONSTRUCTORS and DESTRUCTORS
 
+            // default constructor.
             vector( size_t cap=0 )
             {
-                std::cout << "Entrou no construtor!\n";
-                std::cout << this << "\n";
-
                 m_data = new T[ cap ];
 
                 m_capacity = cap;
@@ -31,11 +33,39 @@ namespace sc {
 
             }
 
+            // range content constructor
+            template< typename InputIt >
+
+            vector( InputIt first, InputIt last )
+            {
+                size_t distance = std::distance( first, last );
+
+                m_data = new T[distance];
+
+                m_capacity = distance;
+
+                m_end =0;
+
+                for( int i = 0; i < distance ; i++ )
+                {
+                    m_data[i] = *(first);
+                    m_end++;
+                    first++;
+                }
+            }
+
+            vector( const vector & other )
+            {
+                
+            }
+
+            // clear elements in array( change logical index ).
             void clear()
             {
                 m_end = 0;
             }
 
+            // Check the free space in array.
             bool empty() const
             {
                 return m_end == 0;
@@ -45,14 +75,114 @@ namespace sc {
             {
                 return m_end == m_capacity;
             }
+            //===============================
 
+            // default destructor
             ~vector()
             {
-                std::cout << "Entrou no destrutor!\n";
-
                 delete[] m_data;
             }
 
+            size_t size() { return m_capacity; }
+            //===================================================================
+
+
+
+            // CLASS ITERATOR ===================================================
+
+            class iterator
+            {
+                private:
+                    T *ptr;
+
+                public:
+
+                    // Basic pointer arithmetic is hardly used here.
+                    // default iterator constructor
+                    iterator( T * pt=nullptr ) : ptr{ pt } {}
+
+                    // default copy construtor ( Itr ptr2( ptr ) )
+                    iterator( const iterator & ) = default;
+
+                    // default destructor
+                    ~iterator( void ) = default;
+
+                    // assign operator ( it = it2 )
+                    iterator& operator=( iterator& rhs ) // right hand side
+                    { this->ptr = rhs.ptr; return *this; }
+
+                    // operator 'pointer to'( *it = value )
+                    T& operator*( void ){ return *ptr; };
+
+                    // sum operator +
+                    T& operator+( size_t offset ) const{ return iterator( ptr + offset ); }
+                    // normal pointer increment is accepted.
+                    // return a new iterator using contructor pointer + integer value.
+
+                    // INCREMENTS ( ++it and it++ )
+                    // ++it
+                    T& operator++( void ) { ptr++; return *this; }
+
+                    // it++
+                    T& operator++( int )
+                    {
+                        iterator temp_clone( *this );
+                        ptr++;
+                        return temp_clone;
+                    }
+
+                    bool operator==( const iterator & rhs )
+                    { return this->ptr == rhs.ptr; }
+
+                    bool operator!=( const iterator & rhs )
+                    { return this->ptr != rhs.ptr; }
+
+
+
+
+
+
+            };
+            //=================================================================
+
+
+
+            // INDEX OPERATOR [] ==============================================
+
+            // without verification
+
+            T & operator[]( size_t index )
+            {
+                return m_data[index];
+            }
+
+            const T & operator[]( size_t index ) const
+            {
+                return m_data[index];
+            }
+
+            // with verification
+
+            T & at( size_t index )
+            {
+                if( index >= m_capacity )
+                  throw std::out_of_range("Position out of range!\n");
+
+                return m_data[index];
+            }
+
+            const T & at( size_t index ) const
+            {
+                if( index >= m_capacity )
+                  throw std::out_of_range("Position out of range!\n");
+
+                return m_data[index];
+            }
+
+
+            // SPACE MANIPULATION============================================
+
+            // reserve space in memory( allocating ).
             void reserve( size_t ncap )
             {
 
@@ -62,7 +192,8 @@ namespace sc {
                     m_capacity = 1;
                 }
 
-                T *temp = new T[ncap];
+                T * temp = new T[ncap];
+
 
                 for( int i = 0; i < m_capacity; i++ )
                 {
@@ -76,6 +207,7 @@ namespace sc {
                 m_capacity = ncap;
             }
 
+            // Insert element in last position and increase logical index.
             void push_back( const T & element )
             {
                 if( full() )
--- src/vector.cpp
+++ src/vector.cpp
@@ -3,15 +3,41 @@
 
 int main(int argc, char *argv[])
 {
+//===================================================================
+//TESTING INITIALIZERS and SIMPLE METHODS
+//===================================================================
+    sc::vector<int> V; // default initializer
+    sc::vector<int> V2(10); // default initializer.
 
-    sc::vector<int> V;
-    sc::vector<int> V2(10);
+    assert( V.empty() ); // TEST ( must be empty )
+    assert( V2.empty() ); // TEST ( must be empty )
 
-    assert( V.empty() );
-    assert( V2.empty() );
+    V.push_back(2); // <vector> push_back method
 
-    V.push_back(2);
+    std::cout << "\n" << V[0] << "\n";
 
-    std::cout << V.m_data[0] << "\n";
+
+    int testArray[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // test array(?) kkkkkk
+
+    sc::vector<int> V3( testArray, testArray + 10 ); // content [first, last ) range initializer
+
+    std::cout << "[ ";
+    for( int i = 0; i < 10; i++ )
+    {
+        std::cout << V3[i] << " ";
+    }
+    std::cout << "]\n";
+
+    assert( !V3.empty() ); // TEST ( must not be empty )
+    assert( V3.full() ); // TEST ( must be full )
+
+    std::cout << "\n>>V size = " << V.size();
+    std::cout << "\n>>V2 size = " << V2.size();
+    V2.push_back(4);
+    std::cout << "\n>>V2 after push_back size = " << V2.size();
+    std::cout << "\n>>V3 size = " << V3.size();
+    V3.push_back(4);
+    std::cout << "\n>>V3 after push_back size = " << V3.size();
+    std::cout << "\n";
     return 0;
 }
